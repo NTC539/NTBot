9247ceddbea56a6ae64bc276b4e3be8756230073
NTC539 <gamingdimon274@gmail.com>
2024-03-16 12:02:44 +0300

    Файл настроек
    Теперь все необходимые файлы лежат в папке внутри Program Files (x86). Также появился файл settings.txt, в котором через запятую записано расписание, т. е. расписание теперь хранится не внутри кода, благодаря чему его можно настраивать, не залезая в код

--- main.py
+++ main.py
@@ -4,14 +4,32 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
+basea = ['Русс','Лите','Алге','Геом','Инфо','Обще','Англ','Биол','Хими','Исто','Физи','Геог','ОБЖ','Веро']
+baseb = ['Физкультура','Разговоры о важном','Индивидуальный проект']
+timetable = [[],[],[],[],[]]
+baseaddress = r"C:\Program Files (x86)\NTBot\baza.txt"
+yesterdayaddress = r"C:\Program Files (x86)\NTBot\yesterday.txt"
 
-timetable = [['ОБЖ','Исто','Физ-ра','Биол','Алге','Веро','Обще'],
-             ['Алге','Физ-ра','Физи','Обще','Лите','Англ','Геом'],
-             ['Русс','Хими','Исто','Русс','Лите','Англ','Индивидуальный проект'],
-             ['Обще','Геог','Физи','Геом','Алге','Инфо','Англ'],
-             ['Разговоры о важном','Физ-ра','Обще','Русс','Геом','Лите','Алге']]
-baza = ['Русс','Лите','Алге','Геом','Инфо','Обще','Англ','Биол','Хими','Исто','Физи','Геог','ОБЖ','Веро']
-
+cfg = open(r"C:\Program Files (x86)\NTBot\settings.txt", 'r').readlines()
+for i in range(0,5):
+    day = cfg[i + 2].split(', ')
+    for n in range(0,7):
+        subjname = ''
+        for symb in day[n]:
+            a = 0
+            if a < 4:
+                subjname += symb
+                a += 1
+            if subjname in basea:
+                timetable[i].append(subjname)
+                break
+            elif day[n] in baseb:
+                timetable[i].append(day[n])
+                break
+timetable = timetable[1:] + [timetable[0]]
+print(timetable)
+channel1id = int(cfg[0])
+channel2id = int(cfg[1])
 def replace_line(line_num, text, txt):
     lines = open(txt, 'r').readlines()
     lines[line_num] = text
@@ -21,13 +39,12 @@ def replace_line(line_num, text, txt):
 
 async def send(txt):
     baza = open(txt, 'r')
-    channel = bot.get_channel(1156612750945026089)
+    channel = bot.get_channel(channel2id)
     bazafile = baza.read()
     await channel.send(bazafile)
 
 def replace():
-    txt = 'дзназавтра.txt'
-    file = open('база.txt', 'r')
+    file = open(baseaddress, 'r')
     bazalines = file.readlines()
     date = datetime.datetime.now()
     dayweek = date.weekday()
@@ -40,21 +57,21 @@ def replace():
     else:
         currentbaza = timetable[dayweek]
         tomorrow = date + datetime.timedelta(days=1)
-    replace_line(0,f"Задание на {tomorrow.strftime('%d.%m')}:\n",txt)
+    replace_line(0,f"Задание на {tomorrow.strftime('%d.%m')}:\n",yesterdayaddress)
     for i in range(0, 14):
-        if baza[i] in currentbaza:
-            if currentbaza.count(baza[i]) > 1:
-                replace_line(currentbaza.index(baza[i],currentbaza.index(baza[i])+1)+1,str(currentbaza.index(baza[i])+1)+". "+bazalines[i], txt)
-            replace_line(currentbaza.index(baza[i])+1,str(currentbaza.index(baza[i])+1)+". "+bazalines[i], txt)
+        if basea[i] in currentbaza:
+            if currentbaza.count(basea[i]) > 1:
+                replace_line(currentbaza.index(basea[i],currentbaza.index(basea[i])+1)+1,str(currentbaza.index(basea[i])+1)+". "+bazalines[i], yesterdayaddress)
+            replace_line(currentbaza.index(basea[i])+1,str(currentbaza.index(basea[i])+1)+". "+bazalines[i], yesterdayaddress)
         else:
             for a in currentbaza:
-                if a not in baza:
+                if a not in basea:
                     missing = a
-                    replace_line(currentbaza.index(missing)+1,f'{(currentbaza.index(missing)+1)}. {missing}\n',txt)
+                    replace_line(currentbaza.index(missing)+1,f'{(currentbaza.index(missing)+1)}. {missing}\n',yesterdayaddress)
 
 @bot.event
 async def on_ready():
-    channel = bot.get_channel(968476610435088441)
+    channel = bot.get_channel(channel1id)
     for thread in channel.threads:
         print(thread.name)
         async for message in thread.history():
@@ -70,9 +87,10 @@ async def on_ready():
                     subjname += symb
                     n += 1
                 for i in range(0,14):
-                    if subjname == baza[i]:
-                        replace_line(i,task,'база.txt')
+                    if subjname == basea[i]:
+                        replace_line(i,task,baseaddress)
     replace()
-    await send('дзназавтра.txt')
+    await send(yesterdayaddress)
 
 bot.run('')
+
